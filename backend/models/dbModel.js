9921b4f4cadeeb593001ce34abd5d19b2d9a5f33
蘇銘凱 <sjs47311@gmail.com>
2024-09-04 10:41:25 +0800

    [調整]雲端部署相關設定

--- backend/models/dbModel.js
+++ backend/models/dbModel.js
@@ -2,8 +2,8 @@
 const { Sequelize } = require("sequelize");
 
 const sequelize = new Sequelize("WORKTIME2", "postgres", "password", {
-  // host: "postgres",
-  host: "localhost",
+  host: "postgres",
+  // host: "localhost",
   dialect: "postgres",
   logging: false, // 關閉 SQL logging
 });
